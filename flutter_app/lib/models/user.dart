bbac0fb67306e290053d49648e333d042a1f13f4
Abthon <walelignabenezer@gmail.com>
2023-09-12 04:47:28 -0700

    Update user model

--- flutter_app/lib/models/user.dart
+++ flutter_app/lib/models/user.dart
@@ -1,25 +1,55 @@
 import 'book.dart';
+import 'dart:io';
 
 class User {
   // Create user's constructor
   User({
-    required this.name,
-    required this.username,
-    required this.imageUrl,
+    this.firstName = '',
+    this.lastName = '',
+    this.username = '',
+    this.email = '',
+    this.password = '',
+    this.imageUrl = '',
     this.favoriteBooks,
+    this.profileImage,
   });
 
-  String name;
+  String firstName;
+  String lastName;
   String username;
+  String email;
+  String password;
   String imageUrl;
   List<Book>? favoriteBooks;
+  File? profileImage;
+
+  User copyWith({
+    String? username,
+    String? firstName,
+    String? lastName,
+    String? email,
+    String? password,
+    File? profileImage,
+  }) {
+    return User(
+      username: username ?? this.username,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      profileImage: profileImage ?? this.profileImage,
+    );
+  }
 
   // Create user's factory constructor
   // Can be accessed like static methods through the class name
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      name: json['name'] as String,
+      firstName: json['firstName'] as String,
+      lastName: json['lastName'] as String,
       username: json['username'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
       imageUrl: json['imageUrl'] as String,
       favoriteBooks: (json['favoriteBooks'] as List)
           .map(
@@ -29,11 +59,14 @@ class User {
     );
   }
 
-  // Convert users object to JSON map
+  // // Convert users object to JSON map
   Map<String, dynamic> toJson() {
     return {
-      'name': name,
+      'firstName': firstName,
+      'lastName': lastName,
       'username': username,
+      'email': email,
+      'password': password,
       'imageUrl': imageUrl,
       'favoriteBooks': favoriteBooks?.map((book) => book.toJson()).toList()
     };
