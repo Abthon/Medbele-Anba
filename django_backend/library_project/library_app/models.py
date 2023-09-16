e036a10343535deb2ad78f5a14d9a7f21930fdb8
Abthon <walelignabenezer@gmail.com>
2023-09-16 08:16:06 -0700

    Modify django's models.py file

--- django_backend/library_project/library_app/models.py
+++ django_backend/library_project/library_app/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager, PermissionsMixin
+from django.contrib.auth.hashers import make_password,check_password
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.hashers import make_password, check_password
+from django.db import models
+
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, user_name, email, password=None, **extra_fields):
+        # Create and save a regular user
+        if not user_name:
+            raise ValueError('The UserName field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, user_name, email, password=None, **extra_fields):
+        # Create and save a superuser
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(user_name, email, password, **extra_fields)
+
+# Custom User Model
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    user_name = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=50, null=False, blank=False)
+    password = models.CharField(max_length=100, null=False, blank=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    profile_image = models.ImageField(upload_to='Images/ProfilePics', null=True)
+    objects = CustomUserManager()
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    USERNAME_FIELD = 'user_name'
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return self.first_name
+
+# Book Model
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    cover_image = models.ImageField(upload_to='Images/BooksCover')
+    inserted_date = models.DateTimeField(auto_now_add=True)
+    quantity = models.PositiveIntegerField(default=0)
+    
+    def __str__(self) -> str:
+        return self.title
+
+# Favorite Model
+class Favorite(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.book.title
+    
+# Borrow Model
+class Borrow(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_date = models.DateField()
+    return_date = models.DateField(null=True, blank=True)
+    quantity_borrowed = models.PositiveIntegerField(default=1)
+    
+    def __str__(self) -> str:
+        return self.book.title
